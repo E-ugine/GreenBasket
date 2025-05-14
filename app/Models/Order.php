0caf99ef2aa97f3f3716c837fbc7b9a0eb65c8b4
E-ugine <agollaeugine@gmail.com>
2025-05-14 15:31:00 +0300

    chore:create models and seeder files

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+namespace App;
+
+use App\Models\User;
+    
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\SoftDeletes;
+
+    class Order extends Model
+    {
+        use SoftDeletes;
+
+        protected $fillable = [
+            'product_id', 'user_id', 'quantity', 'address' 
+        ];
+
+        public function user()
+        {
+            return $this->belongsTo(User::class, 'user_id');
+        }
+
+        public function product()
+        {
+            return $this->belongsToMany(Product::class)->withPivot('quantity')->withTimestamps();
+        }
+
+    }
